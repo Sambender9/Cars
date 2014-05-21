326a552f4bafc2bdfe575a977ed96f3eeb2b50ef
sambender9 <samben789@aol.com>
2014-05-21 10:02:35 -0300

    Cars

---
+++ CarsDotNet/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarsDotNet.Models;
+namespace CarsDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            ViewBag.Message = "Here you will find a list of luxury cars that I hope to own one day";
+            return View();
+        }
+        public ActionResult Mercedes()
+        {
+            ViewBag.Message = "First Class in Luxury";
+            Mercedes myMercedes = new Mercedes("Mercedes", "SLS AMG", "Red");
+            return View(myMercedes);
+        }
+        public ActionResult BMW()
+        {
+            ViewBag.Message = "BMW: The Ultimate Driving Machine";
+            BMW myBmw = new BMW("BMW", "745li", "Black");
+            return View(myBmw);
+        }
+        public ActionResult Lexus()
+        {
+            ViewBag.Message = "Lexus: Pursuit of Perfection";
+            Lexus myLexus = new Lexus("Lexus", "LS 350", "Green");
+            return View(myLexus);
+        }
+        public ActionResult Audi()
+        {
+            ViewBag.Message = "Audi: Truth in Engineering";
+            Audi myAudi = new Audi("Audi", "A8", "Silver");
+            return View(myAudi);
+        }
+        public ActionResult Maybach()
+        {
+            ViewBag.Message = "Maybach: Not so much a name as a philosophy";
+            Maybach myMaybach = new Maybach("Maybach", "Phantom", "Black");
+            return View(myMaybach);
+        }
+    }
+}
---
+++ CarsDotNet/Models/Cars.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarsDotNet.Models
+{
+    public abstract class Cars
+    {
+        int speed = 0;
+        public string Company { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Wheels { get; set; }
+
+        public Cars (string company, string type, string color, string wheels) 
+        {
+            this.Company = company;
+            this.Type = type;
+            this.Color = color;
+            this.Wheels = 4;
+        }
+        protected Cars()
+        {
+
+        }
+        public int accelerate() 
+        {
+            return speed += 5;
+        }
+        public int decelerate()
+        {
+            return speed -= 5;
+        }
+        public string turnLeft()
+        {
+            return "turn left";
+        }
+        public string turnRight()
+        {
+            return "turn right";
+        }
+    }
+    public class BMW : Cars
+    {
+        int speed = 60;
+        public string Company{get; set;}
+        public string Type {get; set;}
+        public string Color {get; set;}
+        //public int Speed {get; set;}
+
+        public BMW (string company, string type, string color)//,int speed
+        {
+            this.Company = "BMW";
+            this.Type = type;
+            this.Color = color;
+            //this.Speed = 60;
+        }
+        protected virtual string sportMode() 
+        {
+            return "You're now in Sport Mode";
+        }
+        //public static whosFaster() 
+        //{
+        //    if (BMW.speed > Mercedes.speed) 
+        //    {
+        //        return BMW.speed;
+        //    }
+        //    else 
+        //    {
+        //        return Mercedes.speed;
+        //    }
+        //}
+    }
+    public class Mercedes : Cars
+    {
+        //int speed = 65;
+        public string Company { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        //public int Speed {get; set;}
+
+        public Mercedes(string company, string type, string color)//,int speed
+        {
+            this.Company = "Mercedes";
+            this.Type = type;
+            this.Color = color;
+            //this.Speed = 65;
+        }
+        public string v12()
+        {
+            return "My engine is a V12!!!";
+        }
+    }
+    public class Lexus : Cars
+    {
+        int speed = 45;
+        public string Company { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Lexus(string company, string type, string color)
+        {
+            this.Company = "Lexus";
+            this.Type = type;
+            this.Color = color;
+        }
+        public string bio()
+        {
+            return "Im a nice car but not as a nice as a Mercedes!!";
+        }
+    }
+    public class Maybach : Cars
+    {
+        int speed = 85;
+        public string Company { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Maybach(string company, string type, string color)
+        {
+            this.Company = "Maybach";
+            this.Type = type;
+            this.Color = color;
+        }
+        public string luxury()
+        {
+            return "I'm the nicest car money can buy!!!";
+        }
+    }
+    public class Audi : Cars
+    {
+        int speed = 45;
+        public string Company { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Audi(string company, string type, string color)
+        {
+            this.Company = "Audi";
+            this.Type = type;
+            this.Color = color;
+        }
+        public string audiBio()
+        {
+            return "Vroom Vroom party starter!!!";
+        }
+    }
+}
